7c8d5a192fdb15950fa8970116d79287ab454740
Sadam1713 <sadam@payzli.com>
2024-10-01 17:40:36 +0500

    Added AttendaceDb Context

---
+++ Data/AttendanceDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AttendanceSystem.Data
+{
+    public class AttendanceDbContext
+    {
+        private readonly string _connectionString;
+
+        public AttendanceDbContext(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+        public NpgsqlConnection GetConnection()
+        {
+            return new NpgsqlConnection(_connectionString);
+        }
+    }
+}
