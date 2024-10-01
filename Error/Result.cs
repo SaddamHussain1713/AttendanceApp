2f6db8412de66ccafc9153683d09d152a6521c45
Sadam1713 <sadam@payzli.com>
2024-10-01 17:47:47 +0500

    Added result pattern for handling errors

---
+++ Error/Result.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AttendanceSystem.Error
+{
+    public class Result<T>
+    {
+        public bool IsSuccess { get; set; }
+        public T Output { get; set; }
+        public string ErrorMessage { get; set; }
+
+        public static Result<T> Success(T output)
+        {
+            return new Result<T>
+            {
+                IsSuccess = true,
+                Output = output
+            };
+        }
+        public static Result<T> Failure(string errorMessage)
+        {
+            return new Result<T>
+            {
+                IsSuccess = false,
+                ErrorMessage = errorMessage
+            };
+        }
+    }
+}
